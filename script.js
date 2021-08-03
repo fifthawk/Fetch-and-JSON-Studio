5258eec00e50b20863c8aa20f284a0b076e50aae
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-08-03 16:18:27 -0500

    added fetch and diaply data to script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// TODO: add code here
+let runThisStuff = () => {
+    let url = 'https://handlers.education.launchcode.org/static/astronauts.json'
+    fetch(url).then(function(response){
+       response.json().then(function(json){
+           let container = document.getElementById('container')
+           for(let i = 0; i < json.length ;i++){
+            json[i].active && console.log(true)
+            container.innerHTML += `<div class="astronaut">
+            <div class="bio">
+            <p>Current Astronaut count = ${i + 1}<p>
+               <h3>${json[i].firstName}</h3>
+               <ul>
+                  <li>Hours in space: ${json[i].hoursInSpace}</li>
+                  <li class = 'active'>Active: ${json[i].active}</li>
+                  <li>Skills: ${(json[i].skills.join(', '))}</li>
+               </ul>
+            </div>
+            <img class="avatar" src="${json[i].picture}">
+            
+         </div>`
+         
+           }
+           
+       }) 
+    })
+    
+}
+
+window.addEventListener('load', runThisStuff)
