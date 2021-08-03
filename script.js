840678b4556887554e34fcd203e473b691eb9e58
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-08-03 16:46:49 -0500

    cleaned up styling a bit

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let runThisStuff = () => {
        response.json().then(function(json){
            let container = document.getElementById('container')
            for(let i = 0; i < json.length ;i++){
-            json[i].active && console.log(true)
+            
             container.innerHTML += `<div class="astronaut">
             <div class="bio">
-            <p>Current Astronaut count = ${i + 1}<p>
+            <h4>Astronaut count: ${i + 1}<h4>
                <h3>${json[i].firstName}</h3>
                <ul>
                   <li>Hours in space: ${json[i].hoursInSpace}</li>
-                  <li class = 'active'>Active: ${json[i].active}</li>
+                  <li ${json[i].active && ('class = "activated"')}>Active: ${json[i].active}</li>
                   <li>Skills: ${(json[i].skills.join(', '))}</li>
                </ul>
             </div>
